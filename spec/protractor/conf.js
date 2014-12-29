2e2f1ec47c0a3c4893857a1f49614800c31c0025
Grace Tan <gtan@nyc-004038-mbp.local>
2014-12-29 17:31:26 -0500

    add jasmine reporter

--- spec/protractor/conf.js
+++ spec/protractor/conf.js
@@ -1,4 +1,9 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['tests/spec.js']
+    specs: ['tests/spec.js'],
+    onPrepare: function() {
+      require('jasmine-reporters');
+      jasmine.getEnv().addReporter(new jasmineReporters.TapReporter());
+      
+    }
 }
